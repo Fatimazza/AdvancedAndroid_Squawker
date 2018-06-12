2ca3b7ef415963f21ed51dfcce853c8c970fb085
Fatima Azzahro <fatima@paypro.id>
2018-06-12 17:01:10 +0700

    [Exercise 4] Firebase Messaging Service - Add new Service which extends FirebaseMessagingService

---
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessageService.java
@@ -0,0 +1,17 @@
+package android.example.com.squawker.fcm;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by fatimazza on 6/12/18.
+ */
+
+/**
+  * Listens for squawk FCM messages both in the background and the foreground and responds
+  * appropriately
+  * depending on type of message
+  */
+
+public class SquawkFirebaseMessageService extends FirebaseMessagingService {
+
+}
