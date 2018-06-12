5a12cceb1bdde6959cf7a53370f5649a90f2b289
Fatima Azzahro <fatima@paypro.id>
2018-06-12 17:49:19 +0700

    [Exercise 4] Firebase Messaging Service - Get data, Send Notification with data message

--- app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessageService.java
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseMessageService.java
@@ -3,6 +3,16 @@ package android.example.com.squawker.fcm;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.example.com.squawker.MainActivity;
+import android.example.com.squawker.R;
+import android.example.com.squawker.provider.SquawkContract;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.Map;
@@ -18,6 +28,13 @@ import java.util.Map;
   */
 
 public class SquawkFirebaseMessageService extends FirebaseMessagingService {
+
+    private static final String JSON_KEY_AUTHOR = SquawkContract.COLUMN_AUTHOR;
+    private static final String JSON_KEY_AUTHOR_KEY = SquawkContract.COLUMN_AUTHOR_KEY;
+    private static final String JSON_KEY_MESSAGE = SquawkContract.COLUMN_MESSAGE;
+    private static final String JSON_KEY_DATE = SquawkContract.COLUMN_DATE;
+
+    private static final int NOTIFICATION_MAX_CHARACTERS = 30;
 
     private static String LOG_TAG = SquawkFirebaseMessageService.class.getSimpleName();
 
@@ -71,7 +88,34 @@ public class SquawkFirebaseMessageService extends FirebaseMessagingService {
      * @param data Map which has the message data in it
      */
     private void sendNotification(Map<String, String> data) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // Create the pending intent to launch the activity
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+            this, 0 /* request code */, intent, PendingIntent.FLAG_ONE_SHOT);
 
+        String author = data.get(JSON_KEY_AUTHOR);
+        String message = data.get(JSON_KEY_MESSAGE);
+
+        // If the message is longer than the max number of characters we want in our
+        // notification, truncate it and add the unicode character for ellipsis
+        if (message.length() > NOTIFICATION_MAX_CHARACTERS) {
+            message = message.substring(0, NOTIFICATION_MAX_CHARACTERS) + "\u2026";
+        }
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+            new NotificationCompat.Builder(this)
+            .setSmallIcon(R.drawable.ic_duck)
+            .setContentTitle(String.format(getString(R.string.notification_message), author))
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0 /* ID of notif */, notificationBuilder.build());
     }
 
 
